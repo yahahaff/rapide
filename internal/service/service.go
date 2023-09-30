304bf7bf963b0e049ed81533eae188f67f792295
shine <yahahaff@qq.com>
2023-09-30 12:55:30 +0800

    删除注释代码

--- internal/service/service.go
+++ internal/service/service.go
@@ -8,10 +8,4 @@ var Entrance = ServiceGroup{}
 
 type ServiceGroup struct {
 	SysService sys.SysGroup
-	//AuthService         sys.AuthService
-	//UserService         sys.UserService
-	//CasbinService       sys.CasbinService
-	//OperationLogService sys.OperationLogService
-	//MenuService         sys.MenuService
-	//SignupService       sys.SignupService
 }
