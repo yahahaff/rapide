aa7acfabe0b8ce5a4fbb53d017889a56442b3138
yahahaff <yahahaff@qq.com>
2024-08-06 17:19:17 +0800

    fix:Meta字段数据为空

--- backend/internal/service/sys/menu_service.go
+++ backend/internal/service/sys/menu_service.go
@@ -7,24 +7,16 @@ import (
 
 type MenuService struct{}
 
-type MenuResponse struct {
-	Component string         `json:"component"`
-	Meta      sys.MenuMeta   `json:"meta"`
-	Name      string         `json:"name"`
-	Path      string         `json:"path"`
-	Redirect  string         `json:"redirect"`
-	Children  []MenuResponse `json:"children,omitempty"`
-}
-
 // GetMenuList 获取菜单列表
 func (m *MenuService) GetMenuList() (data []sys.Menu, err error) {
 	var menus []sys.Menu
-	db := database.DB.Where("parent_id IS NULL").
-		Preload("Children").
-		Preload("Children.Children")
+	db := database.DB.
+		Preload("Meta").
+		Preload("Children.Meta").
+		Preload("Children.Children.Meta")
 
 	// 查询数据
-	if err = db.Find(&menus).Error; err != nil {
+	if err = db.Where("parent_id IS NULL").Find(&menus).Error; err != nil {
 		return nil, err
 	}
 
